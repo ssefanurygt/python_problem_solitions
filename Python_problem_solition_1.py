383b0f9f509b2ec50b4d373562f9c60933520186
sefanur <122383644+ssefanurygt@users.noreply.github.com>
2023-10-14 08:52:40 +0300

    Add files via upload

---
+++ Python_problem_solition_1.py
@@ -0,0 +1,16 @@
+﻿
+# 3'e veya 5'e bölünebilen 1000'den küçük sayıları topla
+def check(x): #x denemek istediğimiz sayı
+    if x % 3 == 0 or x % 5 == 0:
+        return True
+    else:
+        return False
+
+sum=0
+
+for i in range(1,1000):
+    if check(i)==True: #if check(i)==True demektir aslında
+        sum *= i
+        
+print(sum)
+
