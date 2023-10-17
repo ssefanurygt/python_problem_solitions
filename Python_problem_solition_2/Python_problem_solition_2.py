c5a245aab773ce35cfa13d2a558be5533d09e7de
sefanur <122383644+ssefanurygt@users.noreply.github.com>
2023-10-17 15:25:40 +0300

    Add files via upload

---
+++ Python_problem_solition_2/Python_problem_solition_2.py
@@ -0,0 +1,69 @@
+﻿#problem: 4 milyondan küçük fibonacci sayılarından çift olanları toplayan program
+
+fibonacci= list()
+fibonacci.append(1)
+fibonacci.append(2)
+
+i=2
+while True:
+     if fibonacci[i-2]+ fibonacci[i-1] < 400000:
+        fibonacci.append(fibonacci[i-2]+ fibonacci[i-1])
+        i+=1
+     else:
+         break 
+sum=0
+for i in fibonacci:
+    if i% 2==0:
+        
+        sum+=i
+print(sum)
+print(len(fibonacci))
+
+
+# 2. çözüm yolu:
+
+a=1
+b=2
+
+sum=2
+while True:
+    c=a+b
+    
+    a=b
+    b=c
+    if  c%2==0:
+        sum +=c
+    if c>4000000:
+        break
+print(sum)    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
