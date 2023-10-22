9e856b74924501112ef88544c1caea08c824af63
sefanur <122383644+ssefanurygt@users.noreply.github.com>
2023-10-22 22:16:13 +0300

    Add files via upload

---
+++ Python_problem_solition_3/Python_problem_solition_3.py
@@ -0,0 +1,13 @@
+﻿# 1 ocak 1901'den 31 aralık 2000'e kadar kaç sefer ayın ilk günü pazar olur?
+
+from datetime import date,datetime,timedelta
+
+pazar_sayısı=0
+for yıl in range(1901,2001):
+    for ay in range(1,13):
+        if datetime(yıl,ay,1).weekday()==6:
+            pazar_sayısı=pazar_sayısı+1
+            
+print(pazar_sayısı)
+            
+
